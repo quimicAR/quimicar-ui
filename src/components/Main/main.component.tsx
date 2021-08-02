4dd9b0257268ff42aee0e0a692b48c2c8b8e3d76
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-02 10:51:28 -0300

    feat: Main Component

--- src/components/Main/main.component.tsx
+++ src/components/Main/main.component.tsx
@@ -1,4 +1,4 @@
-import Header from 'components/Header/header.component'
+import Header from '../../components/Header/header.component'
 import * as SC from './main.styles'
 
 const Main = () => {
@@ -8,7 +8,7 @@ const Main = () => {
       {/*
 
         1 - Componente PeriodicTable
-          - Montar Grid
+          - Montar Tabela
           - Separar grid-areas pelas familias
           - Componente Atomo, irá ter cor diferente de acordo com a familia (se basear pelo Figma)
       */}
--- src/components/Main/main.styles.ts
+++ src/components/Main/main.styles.ts
@@ -1,18 +1,8 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-  color: #fff;
   width: 100%;
   height: 100%;
   text-align: center;
-`
-
-export const Description = styled.h2`
-  font-size: 2rem;
-  font-weight: 400;
-`
-
-export const Illustration = styled.img`
-  margin-top: 3rem;
-  width: min(30rem, 100%);
+  background-color: ${(props) => props.theme.pallete.bg};
 `

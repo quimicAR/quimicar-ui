257319b772bc204fbf8c0399db6f278fd2ccd1dc
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-02 10:51:49 -0300

    feat: Search Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import { GlobalStyles } from 'styles/globalStyle'
+import { GlobalStyles } from '../styles/globalStyle'
 import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
@@ -7,9 +7,10 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>quimicAR - Átomos em Realidade Aumentada</title>
-        <link rel="shortcut icon" href="/img/atom.png" />
-        <link rel="apple-touch-icon" href="/img/atom.png" />
+        <link rel="shortcut icon" href="/img/atom-light.svg" />
+        <link rel="apple-touch-icon" href="/img/atom-light.svg" />
         <link rel="manifest" href="/manifest.json" />
+
         <meta
           name="description"
           content="A simple template project to work with React, Typescript, Next, Styled Components and Storybook"
@@ -18,6 +19,16 @@ function App({ Component, pageProps }: AppProps) {
           {' '}
           Icon by Dryicons{' '}
         </a> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />

4155ebdcb30032a07598dea66c752c548ca24980
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-02 10:51:37 -0300

    feat: Search Component

--- src/components/Search/search.component.tsx
+++ src/components/Search/search.component.tsx
@@ -1,23 +1,32 @@
 import * as SC from './search.styles'
 import { FiSearch } from 'react-icons/fi'
-import { useTheme } from 'hooks/use-theme'
+import { useTheme } from '../../hooks/use-theme'
+import { useState } from 'react'
 
 interface SearchProps {
   placeholder?: string
+  value?: string
 }
-const Search: React.FC<SearchProps> = ({ placeholder }) => {
+const Search: React.FC<SearchProps> = ({
+  placeholder = 'Search for elements...',
+  value = ''
+}) => {
   const { theme } = useTheme()
+  const [searchState, setSearchState] = useState<string>(value)
+
   return (
-    <SC.Search
-      theme={theme}
-      placeholder={placeholder}
-      endAdornment={
-        <FiSearch
-          color={theme === 'light' ? '#040415' : '#f1ebdd'}
-          fontSize="1.5em"
-        />
-      }
-    />
+    <SC.SearchContainer themeProp={theme}>
+      <SC.Search
+        placeholder={placeholder}
+        value={searchState}
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        onChange={(event: any) => setSearchState(event?.target.value)}
+      />
+      <FiSearch
+        color={theme === 'light' ? '#040415' : '#f1ebdd'}
+        fontSize="2em"
+      />
+    </SC.SearchContainer>
   )
 }
 
--- src/components/Search/search.styles.ts
+++ src/components/Search/search.styles.ts
@@ -1,14 +1,28 @@
 import { Theme } from 'hooks/use-theme'
 import styled from 'styled-components'
-import { Input } from '@material-ui/core'
-interface ThemeStylesProps {
-  theme: Theme
+
+interface SearchProps {
+  themeProp?: Theme
 }
 
-export const Search = styled(Input)<ThemeStylesProps>`
-  height: 35px !important;
-  padding: 0px !important;
+export const SearchContainer = styled.div<SearchProps>`
+  display: flex;
+  align-items: center;
+  height: 35px;
+  width: 230px;
+  background-color: ${(props) =>
+    props.themeProp === 'light'
+      ? 'rgba(24, 31, 39, 0.1)'
+      : 'rgba(250,249,250, 0.1)'};
+  border-radius: 4px;
+  padding: 8px;
+`
+
+export const Search = styled.input`
+  height: 100%;
+  width: auto;
+  padding: 10px;
   border: none;
   background: transparent;
-  color: ${(props) => (props.theme === 'light' ? '#040415' : '#f1ebdd')};
+  color: ${(props) => props.theme.fonts.color};
 `

56bc8b9f98533c576423149cb37099cbdec859b9
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-07 16:07:54 -0300

    feat: index file to export components

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import Atom from './Atom/atom.component'
+import PeriodicTable from './PeriodicTable/periodic-table.component'
+
+import Header from './Header/header.component'
+import Layout from './Layout/layout.component'
+import Modal from './Modal/modal.component'
+import Search from './Search/search.component'
+import Text from './Text/text.component'
+
+export { Atom, PeriodicTable, Header, Layout, Modal, Search, Text }

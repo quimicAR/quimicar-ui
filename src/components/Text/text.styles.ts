44ad52bf3bea009120dd51522ee6bf317b1af5a9
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-08 17:31:29 -0300

    feat: text component

--- src/components/Text/text.styles.ts
+++ src/components/Text/text.styles.ts
@@ -1,9 +1,17 @@
-import styled from 'styled-components'
+import { TextSize, Weight } from 'models/styles'
+import styled, { css } from 'styled-components'
 
-export const Text = styled.div`
-  background-color: red;
-  width: 150px;
-  height: 150px;
-  padding: 3rem;
+interface TextProps {
+  fontSize: TextSize
+  color?: string
+  weight: Weight
+}
+
+export const Text = styled.p<TextProps>`
+  ${({ theme, fontSize, color, weight }) => css`
+    font-size: ${theme.fonts.sizes[fontSize]};
+    font-weight: ${theme.fonts.weight[weight]};
+    color: ${color || theme.fonts.color};
+    word-spacing: 1px;
+  `}
 `
-

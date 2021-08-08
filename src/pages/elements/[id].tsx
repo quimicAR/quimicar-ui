ca6e49c72bb0d2080ac1a4bd8cd7b073a00cb85e
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-08 17:33:59 -0300

    feat: dynamic element page

--- src/pages/elements/[id].tsx
+++ src/pages/elements/[id].tsx
@@ -1,7 +1,9 @@
-import { Layout } from 'components'
-import { IAtom } from 'interfaces/atom'
+import { Content, Layout, Section, Row, ElementHeader } from 'components'
+import useDarkMode from 'hooks/use-dark-theme'
+import { IAtom } from 'models/atom'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { fetcher } from 'services'
+import { FiFileText, FiInfo } from 'react-icons/fi'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   let size = Array.from(Array(119).keys())
@@ -23,12 +25,101 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 }
 
 const Element: NextPage<{ element: IAtom }> = ({ element }) => {
+  const { isDarkMode } = useDarkMode()
+  const {
+    name,
+    summary,
+    symbol,
+    category,
+    atomic_mass,
+    appearance,
+    boil,
+    density,
+    discovered_by,
+    electron_affinity,
+    electron_configuration,
+    electron_configuration_semantic,
+    electronegativity_pauling,
+    melt,
+    molar_heat,
+    named_by,
+    number,
+    period,
+    phase,
+    spectral_img,
+    element_img
+  } = element
+
   return (
     <Layout>
-      <section>
-        <h1>{element.name}</h1>
-        <p>{element.summary}</p>
-      </section>
+      <ElementHeader
+        category={category}
+        atomic_mass={atomic_mass}
+        element_img={element_img}
+        name={name}
+        number={number}
+        symbol={symbol}
+      />
+
+      <Content>
+        <Section
+          icon={
+            <FiFileText
+              color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
+              fontSize="1.3em"
+              style={{ marginRight: '0.7em' }}
+            />
+          }
+          title="Overview"
+        >
+          <Row title="Atomic Number: " text={number} />
+          <Row title="Atom name: " text={name} />
+          <Row title="Summary:" text={summary} />
+          <Row title=" Appearance:" text={appearance} />
+          <Row title=" Discovered:" text={discovered_by as string} />
+          <Row title=" Named by:" text={named_by} />
+        </Section>
+
+        <Section
+          icon={
+            <FiInfo
+              color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
+              fontSize="1.3em"
+              style={{ marginRight: '0.7em' }}
+            />
+          }
+          title="Atomic Properties"
+        >
+          {spectral_img && (
+            <Row text="" title="Spectrum:" imageUrl={spectral_img} />
+          )}
+          <Row title="State: " text={phase} />
+          {/* Temperatures such as boiling points and melting points are given in
+          degrees kelvin */}
+          <Row title="Period: " text={period} />
+          <Row title="Boil: " text={boil} measure="°K" />
+          <Row title="Melt: " text={melt} measure="°K" />
+          {/* molar heat in (mol*K) */}
+          <Row title="Molal Heat: " text={molar_heat} measure="(mol*K)" />
+          {/* Densities are given in g/l for gases and g/cm³ for solids and liquids */}
+          <Row
+            title="Density: "
+            text={density}
+            measure={phase === 'Gas' ? '(g/l)' : '(g/cm³)'}
+          />
+          <Row title="Electronegativity: " text={electronegativity_pauling} />
+          <Row title="Electron Affinity: " text={electron_affinity} />
+          <Row
+            title="Electronic Configuration: "
+            text={electron_configuration}
+          />
+          {/* Elements with a semantic electron configuration marked with a "*" mean that the electron configuration has not yet been confirmed. */}
+          <Row
+            title="Semantic Electronic Configuration: "
+            text={electron_configuration_semantic}
+          />
+        </Section>
+      </Content>
     </Layout>
   )
 }

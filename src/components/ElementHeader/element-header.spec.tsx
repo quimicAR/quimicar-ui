f396f9244805cc43bd6425ee1dce00c43f6ed2b1
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-08 17:28:22 -0300

    feat: element-header component

---
+++ src/components/ElementHeader/element-header.spec.tsx
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react'
+import ElementHeader from './element-header.component'
+describe('<ElementHeader />', () => {
+  it('should render ElementHeader', () => {})
+})

ef75d2e7bdf96634f88e06ea396de3a7a77b052c
Gabriel CM <gabriel.moura@gsw.com.br>
2021-10-20 17:26:28 -0300

    feat: showing only enabled Elements && using ID as key

--- src/components/PeriodicTable/periodic-table.component.tsx
+++ src/components/PeriodicTable/periodic-table.component.tsx
@@ -1,7 +1,7 @@
 import * as SC from './periodic-table.styles'
 import { IAtom } from '../../models/atom'
 import { Atom } from '../../components'
-import { Size } from '../../models/styles'
+import { Size } from '../../styles/styles'
 interface PeriodicTableProps {
   elements: IAtom[]
   size?: Size
@@ -14,18 +14,20 @@ const PeriodicTable: React.FC<PeriodicTableProps> = ({
   return (
     <SC.PeriodicTableContainer>
       {elements.map((element: IAtom) => {
-        const { name, number, symbol, category, xpos, ypos } = element
-        return (
-          <Atom
-            key={name}
-            atomName={name}
-            atomSymbol={symbol}
-            atomicNumber={number}
-            atomGroup={category}
-            position={{ xPos: xpos, yPos: ypos }}
-            size={size}
-          />
-        )
+        const { id, name, number, symbol, category, xpos, ypos, enabled } =
+          element
+        if (enabled)
+          return (
+            <Atom
+              key={id}
+              atomName={name}
+              atomSymbol={symbol}
+              atomicNumber={number}
+              atomGroup={category}
+              position={{ xPos: xpos, yPos: ypos }}
+              size={size}
+            />
+          )
       })}
     </SC.PeriodicTableContainer>
   )

073df7b7e36ae80b73d9fb884a89bfd48804be29
Gabriel CM <gabriel.moura@gsw.com.br>
2021-10-22 20:27:44 -0300

    fix: removed unused components & files

--- src/components/Search/search.spec.tsx
+++
@@ -1,5 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Search from './search.component'
-describe('<Search />', () => {
-  it('should render Search', () => {})
-})
--- src/layouts/Base/base.spec.tsx
+++
@@ -1,5 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Base from './base.component'
-describe('<Base />', () => {
-  it('should render Base', () => {})
-})

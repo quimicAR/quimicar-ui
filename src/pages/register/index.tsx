c3fa2fe8929650168866cb1e921c224211f59efc
Gabriel CM <gabriel.moura@gsw.com.br>
2021-10-28 16:18:23 -0300

    fix: removed no needed BaseLayout from register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,15 +1,13 @@
 import { NextPage } from 'next'
 import { Button, Input, Text } from 'components'
 import useDarkMode from 'hooks/use-dark-theme'
-import { FiAtSign, FiCreditCard, FiLock, FiUser } from 'react-icons/fi'
+import { FiAtSign, FiLock, FiUser } from 'react-icons/fi'
 import Link from 'next/link'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
-import api from 'services'
 import Swal from 'sweetalert2'
 import { useRouter } from 'next/router'
-import { Base } from 'layouts'
 import { createUser } from 'services/users/create'
 
 interface FormData {
@@ -54,7 +52,7 @@ const Register: NextPage = () => {
 
   const handleOnSubmit: SubmitHandler<FormData> = (formValues) => {
     createUser({
-      fullname: `${formValues.firstname} ${formValues.lastname}`,
+      fullName: `${formValues.firstname} ${formValues.lastname}`,
       email: formValues.email,
       password: formValues.password,
       enabled: true
@@ -72,90 +70,84 @@ const Register: NextPage = () => {
   }
 
   return (
-    <Base>
-      <div className="container rounded p-8 items-center sm:w-8/12 md:w-8/12 lg:w-4/12">
-        <div className="flex flex-col gap-2 text-center ">
-          <div className="mb-8">
-            <Text size="xlg">Sign-up</Text>
-          </div>
-          <div className="flex sm:flex-row flex-col gap-2 mt-6">
-            <Input
-              placeholder="First name *"
-              type="text"
-              error={formState.errors.firstname}
-              icon={
-                <FiUser
-                  color={
-                    isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'
-                  }
-                  fontSize="1.3em"
-                />
-              }
-              {...register('firstname')}
-            />
-
-            <Input
-              placeholder="Last name *"
-              type="text"
-              error={formState.errors.lastname}
-              icon={
-                <FiUser
-                  color={
-                    isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'
-                  }
-                  fontSize="1.3em"
-                />
-              }
-              {...register('lastname')}
-            />
-          </div>
-          <Input
-            placeholder="E-mail *"
-            type="email"
-            error={formState.errors.email}
-            icon={
-              <FiAtSign
-                color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
-                fontSize="1.3em"
-              />
-            }
-            {...register('email')}
-          />
-          <Input
-            placeholder="Password *"
-            type="password"
-            error={formState.errors.password}
-            icon={
-              <FiLock
-                color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
-                fontSize="1.3em"
-              />
-            }
-            {...register('password')}
-          />
-          <Input
-            placeholder="Confirm password *"
-            type="password"
-            error={formState.errors.passwordConfirm}
-            icon={
-              <FiLock
-                color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
-                fontSize="1.3em"
-              />
-            }
-            {...register('passwordConfirm')}
-          />
-          <Button
-            label="Register"
-            disabled={!formState.isValid}
-            onClick={handleSubmit(handleOnSubmit)}
-          />
-          <Link href="/login">
-            <Button label="Back" isLink />
-          </Link>
+    <div className="container rounded p-8 items-center sm:w-8/12 md:w-8/12 lg:w-4/12">
+      <div className="flex flex-col gap-2 text-center ">
+        <div className="mb-8">
+          <Text size="xlg">Sign-up</Text>
         </div>
+        <div className="flex sm:flex-row flex-col gap-2 mt-6">
+          <Input
+            placeholder="First name *"
+            type="text"
+            error={formState.errors.firstname}
+            icon={
+              <FiUser
+                color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
+                fontSize="1.3em"
+              />
+            }
+            {...register('firstname')}
+          />
+
+          <Input
+            placeholder="Last name *"
+            type="text"
+            error={formState.errors.lastname}
+            icon={
+              <FiUser
+                color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
+                fontSize="1.3em"
+              />
+            }
+            {...register('lastname')}
+          />
+        </div>
+        <Input
+          placeholder="E-mail *"
+          type="email"
+          error={formState.errors.email}
+          icon={
+            <FiAtSign
+              color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
+              fontSize="1.3em"
+            />
+          }
+          {...register('email')}
+        />
+        <Input
+          placeholder="Password *"
+          type="password"
+          error={formState.errors.password}
+          icon={
+            <FiLock
+              color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
+              fontSize="1.3em"
+            />
+          }
+          {...register('password')}
+        />
+        <Input
+          placeholder="Confirm password *"
+          type="password"
+          error={formState.errors.passwordConfirm}
+          icon={
+            <FiLock
+              color={isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'}
+              fontSize="1.3em"
+            />
+          }
+          {...register('passwordConfirm')}
+        />
+        <Button
+          label="Register"
+          disabled={!formState.isValid}
+          onClick={handleSubmit(handleOnSubmit)}
+        />
+        <Link href="/login">
+          <Button label="Back" isLink />
+        </Link>
       </div>
-    </Base>
+    </div>
   )
 }
 

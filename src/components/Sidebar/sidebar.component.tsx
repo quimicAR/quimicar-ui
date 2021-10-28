a0efb8ae83e9806d8562cca782eee6c69761b30e
Gabriel CM <gabriel.moura@gsw.com.br>
2021-10-28 16:15:35 -0300

    feat: added sidebar animation & logout function

--- src/components/Sidebar/sidebar.component.tsx
+++ src/components/Sidebar/sidebar.component.tsx
@@ -1,11 +1,19 @@
 import { useRouter } from 'next/router'
 import { IconType } from 'react-icons'
-import { FiLock, FiUsers } from 'react-icons/fi'
+import {
+  FiLock,
+  FiUsers,
+  FiLogOut,
+  FiChevronLeft,
+  FiChevronRight
+} from 'react-icons/fi'
 import { GiAtom } from 'react-icons/gi'
 import * as SC from './sidebar.styles'
 import Link from 'next/link'
-import { Url } from 'url'
 import useDarkMode from 'hooks/use-dark-theme'
+import { destroyCookie } from 'nookies'
+import { Text } from 'components'
+import { useState } from 'react'
 
 const iconOptions = {
   light: '/img/atom-light.svg',
@@ -16,16 +24,25 @@ const SidebarButton: React.FC<{
   icon: IconType | JSX.Element
   label: string
   href: string
+  onClick?: () => void
+  hideLabel?: boolean
   isDarkMode: boolean
-}> = ({ icon, label, href, isDarkMode }) => (
+}> = ({ icon, label, href, isDarkMode, onClick, hideLabel }) => (
   <Link href={href}>
     <div
-      className={`sidebar-button ${
-        isDarkMode ? 'bg-gray-800 bg-opacity-20' : 'bg-gray-100'
-      } group`}
+      onClick={onClick}
+      className={`sidebar-animation flex gap-4 items-center  h-12 w-full px-4 cursor-pointer  hover:bg-primary transition-all duration-300 ease-linear group ${
+        isDarkMode ? 'bg-gray-900 ' : 'bg-gray-100'
+      } ${hideLabel ? 'justify-center' : 'justify-start'}`}
     >
       {icon}
-      <span className="sidebar-tooltip group-hover:scale-100">{label}</span>
+      {hideLabel ? (
+        <span className="ml-5 z-20 sidebar-tooltip  group-hover:scale-100">
+          {label}
+        </span>
+      ) : (
+        <Text>{label}</Text>
+      )}
     </div>
   </Link>
 )
@@ -34,47 +51,82 @@ const Sidebar = () => {
   const router = useRouter()
   const { isDarkMode } = useDarkMode()
   const iconColor = isDarkMode ? 'var(--color-light)' : 'var(--color-dark)'
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleLogout = () => {
+    destroyCookie(null, 'quimicar.token')
+    router.reload()
+    router.push('/login')
+  }
+
   return (
     <SC.Sidebar
-      className={`fixed top-0 left-0 h-screen w-16 m-0 flex flex-col ${
-        isDarkMode ? 'bg-gray-900' : 'bg-opacity-100'
-      } text-white shadow-md`}
+      className={`sidebar-animation flex flex-col items justify-center text-white shadow-md bg-gray-100 cursor-pointer
+      ${isOpen ? 'w-60' : 'w-16'}
+       ${isDarkMode ? 'bg-gray-900' : 'bg-opacity-100'} `}
     >
       <div
-        className={`flex h-14 ${
-          isDarkMode ? 'bg-secondary' : 'bg-gray-100 bg-opacity-30'
-        } items-center justify-center`}
+        className={`bg-primary flex h-14 w-full items-center px-4 justify-start  ${
+          isDarkMode ? 'bg-gray-900' : 'bg-gray-100 bg-opacity-30 '
+        } group`}
+        onClick={() => router.push('/')}
       >
-        <div
-          className="cursor-pointer flex hover:rotate-180 transform-gpu transition-all duration-1000 "
-          onClick={() => router.push('/')}
-        >
-          <img
-            className="w-8 h-8"
-            src={isDarkMode ? iconOptions.light : iconOptions.dark}
-            alt="Imagem de um átomo"
+        <img
+          className="atom-icon group-hover:rotate-180 w-7 h-7 mr-4"
+          src={isDarkMode ? iconOptions.light : iconOptions.dark}
+          alt="Imagem de um átomo"
+        />
+        {isOpen && (
+          <Text size="lg" weight="bold">
+            quimicAR
+          </Text>
+        )}
+      </div>
+
+      <div
+        className={`absolute z-10 sidebar-animation ${
+          isOpen ? 'left-56' : 'left-12'
+        } top-10 w-8 h-8 rounded-full flex items justify-center bg-gray-800 shadow-md hover:bg-primary`}
+      >
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <FiChevronLeft /> : <FiChevronRight />}
+        </button>
+      </div>
+
+      <div className="flex flex-col h-full justify-between mt-4 ">
+        <div className="flex flex-col items-start">
+          <SidebarButton
+            icon={<FiUsers color={iconColor} fontSize="1.2em" />}
+            label="Users"
+            href="/users"
+            isDarkMode={isDarkMode}
+            hideLabel={!isOpen}
+          />
+          <SidebarButton
+            icon={<FiLock color={iconColor} fontSize="1.2em" />}
+            label="Roles"
+            href="/roles"
+            isDarkMode={isDarkMode}
+            hideLabel={!isOpen}
+          />
+          <SidebarButton
+            icon={<GiAtom color={iconColor} fontSize="1.2em" />}
+            label="Elements"
+            href="/elements"
+            isDarkMode={isDarkMode}
+            hideLabel={!isOpen}
           />
         </div>
-      </div>
-      <div className="mt-2 flex flex-col gap-1">
-        <SidebarButton
-          icon={<FiUsers color={iconColor} fontSize="1.2em" />}
-          label="Users"
-          href="/users"
-          isDarkMode={isDarkMode}
-        />
-        <SidebarButton
-          icon={<FiLock color={iconColor} fontSize="1.2em" />}
-          label="Roles"
-          href="/roles"
-          isDarkMode={isDarkMode}
-        />
-        <SidebarButton
-          icon={<GiAtom color={iconColor} fontSize="1.2em" />}
-          label="Elements"
-          href="/elements"
-          isDarkMode={isDarkMode}
-        />
+        <div className="mb-4">
+          <SidebarButton
+            icon={<FiLogOut color={iconColor} fontSize="1.2em" />}
+            label="Logout"
+            href="/logout"
+            isDarkMode={isDarkMode}
+            onClick={handleLogout}
+            hideLabel={!isOpen}
+          />
+        </div>
       </div>
     </SC.Sidebar>
   )

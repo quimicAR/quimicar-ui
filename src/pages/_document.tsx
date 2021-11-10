650c483792b4989bbfe1dea563be548ffc4846ce
c0sta <gabriel.moura6@fatec.sp.gov.br>
2021-11-10 17:02:51 -0300

    fix: fixing html lang prop

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="eng">
+      <Html lang="en-US">
         <Head />
         <body>
           <Main />

bd30d9f0a0083fba5da1ca8812590d123ae23172
Gabriel CM <gabrielcostamoura1@gmail.com>
2022-06-26 13:01:31 -0300

    refactor: removed Admin/User layout and abstracted into Base layout

--- src/layouts/Admin/admin.spec.tsx
+++
@@ -1,5 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Admin from './admin.component'
-describe('<Admin />', () => {
-  it('should render Admin', () => {})
-})
--- src/layouts/User/user.spec.tsx
+++
@@ -1,5 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import User from './user.component'
-describe('<User />', () => {
-  it('should render User', () => {})
-})
